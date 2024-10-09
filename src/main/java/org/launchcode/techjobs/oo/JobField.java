c05bca95464cbbc47f75548480c864a81a814e3d
bangoldx <bangold@proton.me>
2024-10-09 17:30:59 -0500

    ALL TEST PASSING GA2 complete

--- src/main/java/org/launchcode/techjobs/oo/JobField.java
+++ src/main/java/org/launchcode/techjobs/oo/JobField.java
@@ -1,9 +1,41 @@
 package org.launchcode.techjobs.oo;
 
-public class JobField {
+import java.util.Objects;
+
+public abstract class JobField {
     private int id;
     private static int nextId = 1;
     private String value;
 
+    public JobField(){
+        id=nextId;
+        nextId++;
+    }
 
+    public JobField(String value){
+        this();
+        this.value=value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString(){
+        return value;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
